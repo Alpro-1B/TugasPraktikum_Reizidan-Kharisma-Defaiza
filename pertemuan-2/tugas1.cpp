fcb08a249ced16c200a4ffd9eaae69e517eb3350
Reizidan Kharisma <144443190+eexzzm@users.noreply.github.com>
2023-10-20 14:38:33 +0700

    Update tugas1.cpp

--- pertemuan-2/tugas1.cpp
+++ pertemuan-2/tugas1.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 using namespace std;
+
 int main () {
 
+    //value awal dari variable 1 dan 2
     int firstValue = 10;
     int secondValue = 8;
-    int thirdValue = 2;
+    int thirdValue;
+    cout<<"value awal dari variable 1 dan 2\nFirst Value = "<<firstValue<<"\nSecond Value = "<<secondValue;
     
-    cout << "First Value = " << firstValue; 
-    cout << "\nSecond Value = " << secondValue;
-    cout << "\nThird Value = " << thirdValue;
 
-    // menggunakan variable tambahan
-        cout << "\n\nFirst Value => 10 - Third Value = " << (firstValue-thirdValue);
-        cout << "\nSecond Value => 8 + Third Value = " << (secondValue+thirdValue);
+        //penukaran nilai menggunakan variable tambahan    
+        thirdValue = firstValue;
+        firstValue = secondValue;
+        secondValue = thirdValue;
+        
+        cout<<"\n\nValue dari variable 1 dan 2 setelah ditukar menggunakan variable tambahan\nFirst Value = "<<firstValue<<"\nSecond Value = "<<secondValue<<endl;
 
-    // tanpa menggunakan variable tambahan
-        cout << "\n\nFirst Value => 10 - 2 = " << (firstValue-2);
-        cout << "\nSecond Value => 8 + 2 = " << (secondValue+2);
+        //mengubah value dari variable 1 dan 2 seperti semula
+        firstValue = 10;
+        secondValue = 8;
+
+        //penukaran value tanpa menggunakan variable tambahan
+        firstValue = (firstValue+secondValue-firstValue);
+        cout << "\nValue dari variable 1 dan 2 setelah ditukar tanpa menggunakan variable tambahan\nfirstValue = " << firstValue;
+
+        //mengubah value dari variable 1 dan 2 seperti semula
+        firstValue = 10;
+        secondValue = 8;
+        secondValue = (secondValue+firstValue-secondValue);
+        cout << "\nSecond Value = " << secondValue;
 
     return 0;
         
